c3aed12f22a0e09f36ea08a4e17a6f80fed90b2d
Anastasia Boruk <anastasia.borukk@gmail.com>
2025-09-16 20:22:11 +0300

    Lesson3

---
+++ src/Lesson3/Lesson3.java
@@ -0,0 +1,86 @@
+package Lesson3;
+
+import java.util.Scanner;
+
+
+public class Lesson3 {
+    public static void main(String[] args) {
+        //Задача 1
+int x=88/(10+12);
+System.out.println("Результат:" +x);
+//Задача 2
+//        Scanner in=new Scanner(System.in);
+//        System.out.println("Введите x1");
+//        int x1=in.nextInt();
+//        System.out.println("Введите y1");
+//        int y1=in.nextInt();
+//        System.out.println("Введите z1");
+//        int z1 =in.nextInt();
+//            x1 += y1;
+//            y1 *= z1;
+//            System.out.println("Result " +x1);
+//            System.out.println("Result " +y1);
+//            System.out.println("Result " +z1);
+// Задача 3
+                //213/x+258/x+60=217
+//int sum1=217-60;
+//        int sum2=213+258;
+//double x2=sum2/sum1;
+//System.out.println("Result x2= "+x2);
+
+// Задача 4
+
+//        int x=5;
+//        int y=2;
+//        int c;
+//        c=x++*y--;
+//        System.out.println("c="+c+ " x="+x+" y="+y );
+
+//Задача 5
+//        Scanner in=new Scanner(System.in);
+//        System.out.println("Введите a");
+//         int a=in.nextInt();
+//         System.out.println("Введите b");
+//        int b=in.nextInt();
+//if (a>b){
+//    int c=(a-b);
+//    System.out.println(c);
+//}
+//else if (a==b){
+//    int d=a+b;
+//    System.out.println(d);
+//    }
+//else  {int m=(b-a);
+//System.out.println(m);
+//}
+
+//Задача 6
+//                Scanner in=new Scanner(System.in);
+//        System.out.println("Введите a");
+//         int a=in.nextInt();
+//         System.out.println("Введите b");
+//        int b=in.nextInt();
+//        if (a>b & b!=0) {
+//            System.out.println(a/b);
+//        }
+//        else if (a!=0){
+//            System.out.println(b/a);
+//        }
+//        else {
+//            System.out.println(a+b);
+//        }
+
+// Задача 7
+        //        Scanner in=new Scanner(System.in);
+//        System.out.println("Введите a");
+//         int a=in.nextInt();
+//        if (a%3==0) {
+//            System.out.println("Число делится на 3");}
+//        else {
+//            System.out.println("Число не делится на 3");
+//        }
+
+
+  }}
+
+
