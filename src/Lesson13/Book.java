c470888cfbcb5e7a0077146c0af5f767e1bd1bd1
Anastasia Boruk <anastasia.borukk@gmail.com>
2025-11-02 17:13:07 +0300

    Lesson 13

---
+++ src/Lesson13/Book.java
@@ -0,0 +1,51 @@
+package Lesson13;
+
+
+public class Book {
+    private String title;
+    private String author;
+
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        Book book = (Book) o;
+        if (title.equals(book.getTitle())) {
+            return true;
+        }
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return author.hashCode() + title.hashCode();
+    }
+
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                '}';
+    }
+}
